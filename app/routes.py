1c0987e14c7b1db044222c56453a0241cb65c85b
AlieniAgustin <alieniagustinweb@gmail.com>
2025-08-11 18:03:57 -0300

    add header and footer

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,4 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def start():
-    conn = get_db()
-    cursor = conn.cursor(dictionary=True) #Devuelve filas como diccionario
-    cursor.execute("SELECT * FROM persona NATURAL JOIN telefono_persona;")  
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('start.html', data=data)
+   return render_template('start.html')
