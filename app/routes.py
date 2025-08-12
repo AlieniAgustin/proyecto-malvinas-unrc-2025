06fdcfae24421ba0638c180a41977972f3639414
AlieniAgustin <alieniagustinweb@gmail.com>
2025-08-11 22:18:22 -0300

    modify header and footer. add routes

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,27 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def start():
-   return render_template('start.html')
+    return render_template('start.html')
+
+@bp.route('/buscar')
+def buscar():
+    return render_template('buscar.html')
+
+@bp.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@bp.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@bp.route('/terminos')
+def terminos():
+    return render_template('terminos.html')
+
+@bp.route('/privacidad')
+def privacidad():
+    return render_template('privacidad.html')
+
+
+
