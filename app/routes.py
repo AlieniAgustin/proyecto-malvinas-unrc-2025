605ceae4c1f04996e76894f14069871be0f835bb
Julian Varea <julianvarea14@gmail.com>
2025-08-27 23:03:36 -0300

    Add: initial buscar.html structure and styles

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from .models.administrador import Administrador
 from werkzeug.security import check_password_hash
 from app.db import get_db
