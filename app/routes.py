083ed25556fdf25de03728feebf5bb59bfb3ed07
camiastrada <camiastradaw@gmail.com>
2025-09-18 11:12:11 -0300

    modify buscar endpoint. add admin search filters

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from .models.administrador import Administrador
 from werkzeug.security import check_password_hash
 from app.db import get_db
@@ -35,9 +35,7 @@ def start():
 def buscar():
     query = """
         SELECT 
-            p.apellido,
-            p.nombre,
-            fza.nombre AS fuerza
+           *
            
         FROM persona p
         JOIN veterano v ON p.dni = v.dni_veterano
@@ -45,6 +43,7 @@ def buscar():
         LEFT JOIN localidad loc ON v.localidad_nacimiento = loc.id_localidad
         LEFT JOIN provincia prov ON loc.id_provincia = prov.id_provincia
         LEFT JOIN fallecido fal ON v.dni_veterano = fal.dni_veterano
+        LEFT JOIN localidad loc_res ON v.localidad_residencia = loc_res.id_localidad
         WHERE 1=1
     """
 
@@ -53,9 +52,9 @@ def buscar():
     # Filtros
     apellido = request.args.get("apellido", "")
     nombre = request.args.get("nombre", "")
-    provincia_id = request.args.get("provincia", "")
-    departamento_id = request.args.get("departamento", "")
-    localidad_id = request.args.get("localidad", "")
+    provincia_nacimiento_id = request.args.get("provincia_nacimiento", "")
+    departamento_nacimiento_id = request.args.get("departamento_nacimiento", "")
+    localidad_nacimiento_id = request.args.get("localidad_nacimiento", "")
     fuerza_id = request.args.get("fuerza", "")
     vf = request.args.get("vf", "") # 1 Vivo - 0 Fallecido
     
@@ -69,17 +68,17 @@ def buscar():
         query += " AND LOWER(p.nombre) LIKE %s"
         params.append("%" + nombre.lower() + "%")
 
-    if departamento_id:
+    if departamento_nacimiento_id:
         query += " AND LOWER(loc.departamento) LIKE %s"
-        params.append("%" + departamento_id.lower() + "%")
+        params.append("%" + departamento_nacimiento_id.lower() + "%")
 
-    if provincia_id:
+    if provincia_nacimiento_id:
         query += " AND loc.id_provincia = %s"
-        params.append(provincia_id)
+        params.append(provincia_nacimiento_id)
 
-    if localidad_id:
+    if localidad_nacimiento_id:
         query += " AND loc.id_localidad = %s"
-        params.append(localidad_id)
+        params.append(localidad_nacimiento_id)
 
     if fuerza_id:
         query += " AND v.id_fuerza= %s"
@@ -91,14 +90,51 @@ def buscar():
         elif vf in ["1", "vivo"]:
             query += " AND fal.dni_veterano IS NULL"       
 
+ # Filtros restringidos a administradore
+    if current_user.is_authenticated:
+        dni = request.args.get("dni", "")
+        provincia_residencia_id = request.args.get("provincia_residencia", "")
+        localidad_residencia_id = request.args.get("localidad_residencia", "")
+        departamento_residencia = request.args.get("departamento_residencia", "")
+        mes_cumple = request.args.get("mes_cumple", "")
+        datos_incompletos = request.args.get("incompletos", "")
+
+        if dni:
+            query += " AND p.dni = %s"
+            params.append(dni)
+
+        if provincia_residencia_id:
+            query += " AND loc_res.id_provincia = %s"
+            params.append(provincia_residencia_id)
+
+        if departamento_residencia:
+            query += " AND LOWER(loc_res.departamento) = %s"
+            params.append(departamento_residencia.lower())
+
+        if localidad_residencia_id:
+            query += " AND v.localidad_residencia = %s"
+            params.append(localidad_residencia_id)
+
+        if mes_cumple:
+            query += " AND MONTH(v.fecha_nacimiento) = %s"
+            params.append(mes_cumple)
+
+        if datos_incompletos:
+            query += """ 
+                AND (
+                    p.apellido IS NULL OR
+                    p.nombre IS NULL OR
+                    v.id_fuerza IS NULL OR
+                    v.fecha_nacimiento IS NULL
+                )
+            """
     conn = get_db()
     cursor = conn.cursor(dictionary=True)
     cursor.execute(query, params)
     resultados = cursor.fetchall()
     conn.close()
 
-    return {"resultados": resultados}
-    # return render_template('buscar.html')
+    return render_template('buscar.html')
 
 @bp.route('/admin')
 @login_required
