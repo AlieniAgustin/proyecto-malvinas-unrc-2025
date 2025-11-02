256e1cf46aad7dc2362b778f133e15d6fd9daebc
Julian Varea <julianvarea14@gmail.com>
2025-11-01 21:36:20 -0300

    fix: encoding issue

--- app/db.py
+++ app/db.py
@@ -8,8 +8,14 @@ def get_db():
             host=current_app.config['MYSQL_HOST'],
             user=current_app.config['MYSQL_USER'],
             password=current_app.config['MYSQL_PASSWORD'],
-            database=current_app.config['MYSQL_DB']
+            database=current_app.config['MYSQL_DB'],
+            charset='utf8mb4',
+            use_unicode=True
         )
+        g.db.cursor().execute("SET NAMES utf8mb4;")
+        g.db.cursor().execute("SET CHARACTER SET utf8mb4;")
+        g.db.cursor().execute("SET character_set_connection=utf8mb4;")
+        
     return g.db
 
 def close_db(e=None):
